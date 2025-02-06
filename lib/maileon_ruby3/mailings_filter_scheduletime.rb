b66e1f65756195a22f17c84633b1b0fd78a04ef1
Florian Jäckel <mail@florianjaeckel.de>
2025-02-06 17:43:52 +0100

    add new newsletter schedule time api endpoint

---
+++ lib/maileon_ruby3/mailings_filter_scheduletime.rb
@@ -0,0 +1,53 @@
+module MaileonRuby3
+  class MailingsFilterScheduletime < Resource
+    require 'nokogiri'
+
+    DEFAULT_STANDARD_FIELDS = %w[state type name scheduleTime]
+
+    def initialize(apikey: nil, debug: false)
+      super(apikey: apikey, debug: debug)
+
+      @url = "mailings/filter/scheduletime"
+    end
+
+    def get(schedule_time: nil)
+      response = @session.get(:path => "#{@path}#{@url}#{get_parameters(schedule_time: schedule_time)}", :headers => get_headers_xml)
+      return unless response[:status] == 200
+      parse_body(response[:body])
+    end
+
+    def create
+      raise RuntimeError('Not implemented.')
+    end
+
+    def update
+      raise RuntimeError('Not implemented.')
+    end
+
+    def delete
+      raise RuntimeError('Not implemented.')
+    end
+
+    def get_parameters(method: METHOD_GET, standard_fields: DEFAULT_STANDARD_FIELDS, schedule_time:)
+      raise RuntimeError('scheduleTime must be a DateTime') if schedule_time.class != DateTime
+      r = "?scheduleTime=#{CGI::escape(schedule_time.strftime('%Y-%m-%d %H:%M:%S'))}&beforeSchedulingTime=false&"
+      standard_fields.each { |field| r = r + "fields=#{CGI::escape(field)}&"}
+      r.chomp('&')
+    end
+
+    def parse_body(body)
+      xml = Nokogiri::XML(body)
+      mailings = []
+
+      xml.xpath('/mailings/mailing').each do |m|
+        mailing = {}
+        mailing[:id] = m.at_xpath('id').content
+        m.xpath('fields/field').each do |field|
+          mailing[field.at_xpath('name').content.to_sym] = field.at_xpath('value').content
+        end
+        mailings.push(mailing)
+      end
+      mailings
+    end
+  end
+end
--- lib/maileon_ruby3/version.rb
+++ lib/maileon_ruby3/version.rb
@@ -1,3 +1,3 @@
 module MaileonRuby3
-  VERSION = "0.9.11"
+  VERSION = "0.10"
 end
